0d7059334920e0921a10275ac20399ace1070434
Devender Singh <dkathait269@gmail.com>
2023-07-10 11:29:29 +0530

    Array Practice

---
+++ Strings/1. reverse_string.py
@@ -0,0 +1,9 @@
+# Question: Reverse a string.
+# Created by Devender Singh
+
+def reverse_string(s):
+    return s[::-1]
+
+# Driver code
+input_str = "hello world"
+print("Reversed string:", reverse_string(input_str))
