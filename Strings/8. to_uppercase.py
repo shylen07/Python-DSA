e08473b1cb41e104739c0236efdca07ee91a3581
Devender Singh <dkathait269@gmail.com>
2023-07-17 11:29:47 +0530

    Array Practice

---
+++ Strings/8. to_uppercase.py
@@ -0,0 +1,9 @@
+# Question: Convert a string to uppercase.
+# Created by Devender Singh
+
+def to_uppercase(s):
+    return s.upper()
+
+# Driver code
+input_str = "Hello, World!"
+print("Uppercase string:", to_uppercase(input_str))
