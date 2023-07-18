e93cc4b1eb92d3c99f397e3cce0dcf6b68e5fa49
Devender Singh <dkathait269@gmail.com>
2023-07-18 11:29:50 +0530

    Array Practice

---
+++ Strings/9. to_lowercase.py
@@ -0,0 +1,9 @@
+# Question: Convert a string to lowercase.
+# Created by Devender Singh
+
+def to_lowercase(s):
+    return s.lower()
+
+# Driver code
+input_str = "Hello, World!"
+print("Lowercase string:", to_lowercase(input_str))
